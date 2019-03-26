a358981c8ba3e4054623ac1dc0845ea68ed62364
Fernando Crespo <fernando82@gmail.com>
2019-03-26 16:59:08 -0300

    update deploy script

--- scripts/ghp-deploy.sh
+++ scripts/ghp-deploy.sh
@@ -1,14 +1,13 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 CNAME=`cat $SCRIPTPATH/../src/CNAME`
-ng build -prod -bh "https://$CNAME"
+ng build --prod --baseHref "https://$CNAME"
 
 OUT=$? #exit code from last command
 if [ $OUT -eq 0 ] ; then
     read -p 'Deploy? [y|N] ' deploy
     case $deploy in
         y|Y) echo "Deploying";
-        cd $SCRIPTPATH/../
-        angular-cli-ghpages;;
+        $SCRIPTPATH/..//node_modules/angular-cli-ghpages/bin/angular-cli-ghpages;;
         *) echo "Not deploying";;
     esac
 fi
