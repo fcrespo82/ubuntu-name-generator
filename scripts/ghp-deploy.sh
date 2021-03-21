f19ce954d15c084afe3945baa4873c622e8ea733
Fernando Crespo <fernando82@gmail.com>
2021-03-21 00:39:56 -0300

    Changed deploy to 'ng deploy'

--- scripts/ghp-deploy.sh
+++ scripts/ghp-deploy.sh
@@ -1,13 +1,1 @@
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-CNAME=`cat $SCRIPTPATH/../src/CNAME`
-ng build --prod --baseHref "https://$CNAME"
-
-OUT=$? #exit code from last command
-if [ $OUT -eq 0 ] ; then
-    read -p 'Deploy? [y|N] ' deploy
-    case $deploy in
-        y|Y) echo "Deploying";
-        $SCRIPTPATH/../node_modules/angular-cli-ghpages/angular-cli-ghpages;;
-        *) echo "Not deploying";;
-    esac
-fi
+echo "Deprecated, use \`ng deploy\` instead"
